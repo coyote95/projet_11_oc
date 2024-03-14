1e7e7104c31bb0ac716ba3845e3f611004eec9d1
coyote95 <marie.guillot.oc@gmail.com>
2024-03-14 17:30:37 +0100

    add test unitaire load json file

---
+++ tests/unit_tests/test_json_file.py
@@ -0,0 +1,20 @@
+import pytest
+from projet_11_oc.server import loadClubs, loadCompetitions
+
+def test_loadClubs():
+    clubs = loadClubs()
+    assert isinstance(clubs, list)
+    assert len(clubs) > 0
+    for club in clubs:
+        assert 'name' in club
+        assert 'email' in club
+        assert 'points' in club
+
+def test_loadCompetitions():
+    competitions = loadCompetitions()
+    assert isinstance(competitions, list)
+    assert len(competitions) > 0
+    for competition in competitions:
+        assert 'name' in competition
+        assert 'date' in competition
+        assert 'numberOfPlaces' in competition
