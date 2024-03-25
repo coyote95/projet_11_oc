1ec968348f941dbeeb876d0f1e045dcb830bf380
coyote95 <marie.guillot.oc@gmail.com>
2024-03-25 12:26:54 +0100

    test_purchase unallowed point with monkeypatch

--- tests/unit_tests/test_purchaseplaces.py
+++ tests/unit_tests/test_purchaseplaces.py
@@ -8,6 +8,7 @@ def test_purchase_places_status_code(client, fixture_data):
         response = client.post('/purchasePlaces', data=fixture_data)
         assert response.status_code == 200
 
+
 def test_purchase_places_points_allowed(client, fixture_data, monkeypatch):
     print(fixture_data)
 
@@ -26,3 +27,18 @@ def test_purchase_places_points_allowed(client, fixture_data, monkeypatch):
     assert b'Great-booking complete!' in response.data
 
 
+def test_purchase_places_points_unallowed(client, fixture_data, monkeypatch):
+    print(fixture_data)
+
+    # Set the desired values for the global variables
+    mocked_clubs = [{"name": "Simply Lift", "points": 2}]   # 6 places in the fixture
+    mocked_competitions = [{"name": "Spring Festival", "numberOfPlaces": 70}]
+
+    # Use monkeypatch to substitute the global variables
+    monkeypatch.setattr('projet_11_oc.server.clubs', mocked_clubs)
+    monkeypatch.setattr('projet_11_oc.server.competitions', mocked_competitions)
+
+    response = client.post('/purchasePlaces', data=fixture_data)
+
+    print (response.data)
+    assert b'Error: Insufficient places available for booking!'in response.data #6places in the fixture
